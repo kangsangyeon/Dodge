64418587edd0ca7b384d5e23497df509d222a49e
DESKTOP-UMGRHG6\sanyoni <ksyline1997@gmail.com>
2021-10-23 08:49:58 +0900

    Player 구조체를 정의하였습니다.

---
+++ Dodge/Player.c
@@ -0,0 +1,32 @@
+﻿#include "Player.h"
+
+#include <stdlib.h>
+
+Player* Player_Create(wchar_t* _spriteFilePath, Vector2D _pivot, Vector2D _position, float _moveSpeed)
+{
+	Player* _player = (Player*)malloc(sizeof(Player));
+	_player->worldObject = WorldObject_CreateWithTextFile(_spriteFilePath, _pivot, _position);
+	_player->moveSpeed = _moveSpeed;
+
+	return _player;
+}
+
+void Player_Release(Player* _player)
+{
+	if (_player == NULL)
+		return;
+
+	if (_player->worldObject != NULL)
+		WorldObject_Release(_player->worldObject);
+
+	free(_player);
+}
+
+void Player_Move(Player* _player, Vector2D _vector, float _deltaTime)
+{
+	if (Vector2D_IsEquals(_vector, Vector2D_Zero) == true)
+		return;
+
+	Vector2D _velocity = Vector2D_Multiply(_vector, _player->moveSpeed * _deltaTime);
+	_player->worldObject->position = Vector2D_Add(_player->worldObject->position, _velocity);
+}
---
+++ Dodge/Player.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include "WorldObject.h"
+
+typedef struct TPlayer
+{
+	WorldObject* worldObject;
+	float moveSpeed;
+} Player;
+
+Player* Player_Create(wchar_t* _spriteFilePath, Vector2D _pivot, Vector2D _position, float _moveSpeed);
+
+void Player_Release(Player* _player);
+
+void Player_Move(Player* _player, Vector2D _vector, float _deltaTime);
