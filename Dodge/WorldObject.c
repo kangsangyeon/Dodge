2bc84a033bea9125dd5f5a23dd43f3fe98ae74e5
DESKTOP-UMGRHG6\sanyoni <ksyline1997@gmail.com>
2021-10-23 07:33:27 +0900

    월드상에 존재하는 오브젝트를 표현하는 구조체인 WorldObject 구조체를 정의하였습니다.

---
+++ Dodge/WorldObject.c
@@ -0,0 +1,31 @@
+﻿#include "WorldObject.h"
+
+#include <stdlib.h>
+#include "Sprite.h"
+
+WorldObject* WorldObject_Create(Sprite* _sprite, Vector2D _position)
+{
+	WorldObject* _outWorldObject = (WorldObject*)malloc(sizeof(WorldObject));
+	_outWorldObject->sprite = _sprite;
+	_outWorldObject->position = _position;
+
+	return _outWorldObject;
+}
+
+WorldObject* WorldObject_Create(wchar_t* _spriteTextFilePath, Vector2D _position)
+{
+	Sprite* _sprite = Sprite_LoadFromTextFile(_spriteTextFilePath);
+
+	return WorldObject_Create(_sprite, _position);
+}
+
+void WorldObject_Release(WorldObject* _worldObject)
+{
+	if (_worldObject == NULL)
+		return;
+
+	if (_worldObject->sprite != NULL)
+		Sprite_Release(_worldObject->sprite);
+
+	free(_worldObject);
+}
---
+++ Dodge/WorldObject.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include "Sprite.h"
+#include "Vector2D.h"
+
+typedef struct TWorldObject
+{
+	Sprite* sprite;
+	Vector2D position;
+} WorldObject;
+
+WorldObject* WorldObject_Create(Sprite* _sprite, Vector2D _position);
+
+WorldObject* WorldObject_Create(wchar_t* _spriteTextFilePath, Vector2D _position);
+
+void WorldObject_Release(WorldObject* _worldObject);
